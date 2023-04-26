0087aca8678f1efb0d6e5f9856c2e04a22db8164
luisaque <131699968+luisaque@users.noreply.github.com>
2023-04-26 20:57:09 +0200

    Update HelloController.java

--- src/main/java/com/example/pps4/HelloController.java
+++ src/main/java/com/example/pps4/HelloController.java
@@ -3,12 +3,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController{
-@GetMapping("/hello")
-    public String hello() {
-        return "Hello luis";
-    }
-@GetMapping("/goodbye")
-public String goodbye() {
-    return "Goodbye luis";
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello luis";
+    }
+    @GetMapping("/goodbye")
+    public String goodbye(){
+        return "Goodbye luis";
     }
 }
